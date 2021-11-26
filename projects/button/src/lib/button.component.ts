099a5b53063f330c0fdbccdc15ccd527285b817c
Yhoan Galeano <yhoangaleano@gmail.com>
2021-11-25 20:43:18 -0500

    refactor: remove ctor and init button component

--- projects/button/src/lib/button.component.ts
+++ projects/button/src/lib/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-button',
@@ -10,11 +10,4 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class ButtonComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ButtonComponent {}
