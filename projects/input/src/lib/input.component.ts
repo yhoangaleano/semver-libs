52d1be027a226d57846fe0c204cad1a9a25499bf
Yhoan Galeano <yhoangaleano@gmail.com>
2021-11-25 23:11:15 -0500

    fix: add style property

--- projects/input/src/lib/input.component.ts
+++ projects/input/src/lib/input.component.ts
@@ -2,18 +2,22 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-input',
-  template: ` <input [type]="type" name="[name]" id="[id]" [class]="styleClass" /> `,
+  template: `
+    <input [type]="type" name="[name]" id="[id]" [class]="styleClass" [style]="style" />
+  `,
   styles: [],
 })
 export class InputComponent {
   @Input() type: string;
   @Input() name: string;
   @Input() id: string;
-  @Input() styleClass?:  string;
+  @Input() styleClass?: string;
+  @Input() style?: { [key: string]: string };
 
   constructor() {
     this.type = 'text';
     this.name = 'inputName';
     this.id = 'inputId';
+    this.style = { margin: '10px', padding: '10px' };
   }
 }
