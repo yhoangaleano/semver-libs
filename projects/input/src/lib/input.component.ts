ff1647d2bac6931032b02dcb07dac09e8494563e
Yhoan Galeano <yhoangaleano@gmail.com>
2021-11-25 23:15:52 -0500

    feat(input): add content and label for input

--- projects/input/src/lib/input.component.ts
+++ projects/input/src/lib/input.component.ts
@@ -3,21 +3,34 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'lib-input',
   template: `
-    <input [type]="type" name="[name]" id="[id]" [class]="styleClass" [style]="style" />
+    <div [class]="contentClass">
+      <label [class]="labelClass">{{ label }}</label>
+      <input
+        [type]="type"
+        name="[name]"
+        id="[id]"
+        [class]="inputClass"
+        [style]="inputStyle"
+      />
+    </div>
   `,
   styles: [],
 })
 export class InputComponent {
+  @Input() label: string;
   @Input() type: string;
   @Input() name: string;
   @Input() id: string;
-  @Input() styleClass?: string;
-  @Input() style?: { [key: string]: string };
+  @Input() contentClass?: string;
+  @Input() labelClass?: string;
+  @Input() inputClass?: string;
+  @Input() inputStyle?: { [key: string]: string };
 
   constructor() {
+    this.label = "Input label"
     this.type = 'text';
     this.name = 'inputName';
     this.id = 'inputId';
-    this.style = { margin: '10px', padding: '10px' };
+    this.inputStyle = { margin: '10px', padding: '10px' };
   }
 }
