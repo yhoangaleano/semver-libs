71df06431f9c0d41bdd71498466487ffd2e9f217
Yhoan Galeano <yhoangaleano@gmail.com>
2021-11-25 22:30:06 -0500

    refactor(input): support multiple types in input
    BREAKING CHANGE: input component support multiple types

--- projects/input/src/lib/input.component.ts
+++ projects/input/src/lib/input.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-input',
-  template: ` <input type="date" name="inputDate" id="inputDate"> `,
+  template: ` <input [type]="type" name="[name]" id="[id]" /> `,
   styles: [],
 })
-export class InputComponent {}
+export class InputComponent {
+  @Input() type: string;
+  @Input() name: string;
+  @Input() id: string;
+
+  constructor() {
+    this.type = 'text';
+    this.name = 'inputName';
+    this.id = 'inputId';
+  }
+}
