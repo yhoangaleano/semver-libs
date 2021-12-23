d2361a0e600de1b1d7f5631e2d62add8e6c3d5fa
Yhoan Galeano <yhoangaleano@gmail.com>
2021-12-23 16:02:20 -0500

    fix(generic-library): modify component

--- projects/generic-library/src/lib/generic-library.component.ts
+++ projects/generic-library/src/lib/generic-library.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'lib-generic-library',
   template: `
     <p>
-      This is a patch change
+      This is a patch change with example xxxxxx
     </p>
   `,
   styles: [
